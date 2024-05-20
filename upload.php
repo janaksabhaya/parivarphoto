82f0079b739928abc36b23b11c7e665d4266439d
vivek-sheladiya <67312712+vivek-sheladiya@users.noreply.github.com>
2024-05-20 15:42:30 +0530

    sdf

--- upload.php
+++ upload.php
@@ -4,11 +4,14 @@
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
-    // Get the uploaded files
+    $name = $_POST['name'];
+    $villageName = $_POST['villageName'];
+    $idd = $_POST['idd'];
+
     $baseImagePath = 'images/sultanpur-gam.png';
-    $overlayImagePath = $_FILES['image']['tmp_name'];
+    $overlayImagePath = $_FILES['image'];
     
-    // Load the base image
+
     $baseImage = imagecreatefromstring(file_get_contents($baseImagePath));
     if ($baseImage === false) {
         http_response_code(400);
@@ -16,7 +19,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
         exit;
     }
 
-    // Load the overlay image
     $overlayImage = imagecreatefromstring(file_get_contents($overlayImagePath));
     if ($overlayImage === false) {
         http_response_code(400);
@@ -24,31 +26,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
         exit;
     }
 
-    // Get dimensions of the images
     $baseWidth = imagesx($baseImage);
     $baseHeight = imagesy($baseImage);
     $overlayWidth = imagesx($overlayImage);
     $overlayHeight = imagesy($overlayImage);
 
-    // // Define the position where you want to place the overlay image
-    // $dst_x = ($baseWidth - $overlayWidth) / 2; // Center horizontally
-    // $dst_y = ($baseHeight - $overlayHeight) / 2; // Center vertically
-
-    // Merge the overlay image onto the base image
     imagecopy($baseImage, $overlayImage,10 ,50 , 10, 10, $overlayWidth, $overlayHeight);
 
-    // Save the resulting image to a temporary file
     $outputPath = 'final/' . uniqid() . '.png';
     if (!file_exists('final')) {
         mkdir('final', 0777, true);
     }
     imagepng($baseImage, $outputPath);
 
-    // Free up memory
     imagedestroy($baseImage);
     imagedestroy($overlayImage);
 
-    // Return the resulting image as a URL
+
     echo json_encode(['output_image' => $outputPath]);
 } else {
     http_response_code(400);
